f8dd541f5126b98949f316c23935db090908a489
mbclr3 <mbclr3@gatech.edu>
2020-10-11 01:59:14 -0400

    Update bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,15 +1,22 @@
 javascript: (() => {
-    const tw = document.createTreeWalker(
-        document
-            .getElementsByClassName('autograderResultsContainer--body')
-            .item(0),
-        NodeFilter.SHOW_TEXT,
-    );
-    let curr = tw.nextNode();
-    let out = '';
-    while (curr) {
-        out = `${out}\n${curr.textContent}`;
-        curr = tw.nextNode();
-    }
-    navigator.clipboard.writeText(out);
+  const body = document
+    .getElementsByClassName('autograderResultsContainer--body')
+    .item(0);
+  if (body == null) {
+    const header = document
+      .getElementsByClassName('autograderResultsContainer--header')
+      .item(0);
+    header.querySelector('button').click();
+  }
+  const tw = document.createTreeWalker(
+    document.getElementsByClassName('autograderResultsContainer--body').item(0),
+    NodeFilter.SHOW_TEXT
+  );
+  let curr = tw.nextNode();
+  let out = '';
+  while (curr) {
+    out = `${out}\n${curr.textContent}`;
+    curr = tw.nextNode();
+  }
+  navigator.clipboard.writeText(out);
 })();
